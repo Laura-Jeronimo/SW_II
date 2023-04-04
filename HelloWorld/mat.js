a99f186a2360a2a2cf5432d64c83c5921cada0f6
Laura Esther Correia Jeronimo <79860690+Laura-Jeronimo@users.noreply.github.com>
2023-04-04 08:00:21 -0300

    Add files via upload

---
+++ HelloWorld/mat.js
@@ -0,0 +1,8 @@
+var num = 963
+var num2 = 52
+
+function soma(a,b){
+    return a + b;
+}
+
+console.log(soma(num,num2))
---
+++ HelloWorld/nome.js
@@ -0,0 +1,5 @@
+var nome1 = "Laura Esther"
+var nome2 = "Richard Dutra"
+
+console.log(nome1 + " e " + nome2 + " fizeram este código");
+
